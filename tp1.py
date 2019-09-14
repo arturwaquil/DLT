2f349a3bb3a4b0b3dc0016046f4a2b37e9cd1ced
Nicolas <nicolas.duranti@hotmail.com>
2019-09-14 01:18:31 -0300

    Obtencao da matriz P

--- tp1.py
+++ tp1.py
@@ -1,69 +1,114 @@
 import cv2
 import numpy as np
 
+clicked = False
+cursorX = 0
+cursorY = 0
 
-""" Pixel Coordinates: """
+def mouse_callback(event,x,y,flags,param):
+    if event == cv2.EVENT_FLAG_LBUTTON:
+        clicked = True
+        global cursorX, cursorY
+        cursorX = x
+        cursorY = y        
+        
+def window_is_open(windowname):
+    return True if cv2.getWindowProperty(windowname, cv2.WND_PROP_VISIBLE) >= 1 else False
 
-# PeqArea SupDir 473,99
-pc1 = [99, 473]
-# PeqArea SupEsq 508,177
-pc2 = [177, 508]
-# PeqArea InfDir 577,99
-pc3 = [99, 577] 
-# GraArea SupEsq 265,237
-pc4 = [237, 265]
-# GraArea SupDir 267,61
-pc5 = [61, 267]
-# GraArea InfDir 551,61
-pc6 = [61, 551]
-# Campo   InfDir 525,23
-pc7 = [23, 525]
+#Direct Linear Transformation
+def dlt(pixelCoords, worldCoords):
+    if len(pixelCoords) != len(worldCoords):
+        print("Erro: Quantidade diferente de pontos.")
+        return []
+    else:    
+        numPoints = len(worldCoords)
 
-pixelCoords = [pc1, pc2, pc3, pc4, pc5, pc6, pc7]
+    A = []
 
+    for i in range(numPoints): 
+        x, y, z, w = worldCoords[i][0], worldCoords[i][1], worldCoords[i][2], worldCoords[i][3] 
+        u, v = pixelCoords[i][0], pixelCoords[i][1]
+        A.append([x, y, z, w, 0, 0, 0, 0, -u*x, -u*y, -u*z, -u])
+        A.append([0, 0, 0, 0, x, y, z, w, -v*x, -v*y, -v*z, -v])
+    
+    U, S, Vh = np.linalg.svd(A)
 
-""" World Coordinates: """
-#Origem na trave direita, X paralelo à linha de fundo e Y paralelo à lateral
-# wc = [x, y, x, w]
-
-# PeqArea SupDir
-wc1 = [0, 5.5, 0, 1] 
-# PeqArea SupEsq
-wc2 = [-18.32, 5.5, 0, 1] 
-# PeqArea InfDir 
-wc3 = [0, 0, 0, 1]
-# GraArea SupEsq
-wc4 = [-29.32, 16.5, 0, 1] 
-# GraArea InfDir 
-wc5 = [11, 0, 0, 1]
-# GraArea SupDir 
-wc6 = [11, 16.5, 0, 1]
-# Campo   InfDir 
-wc7 = [24.84, 0, 0, 1]
-
-worldCoords = [wc1, wc2, wc3, wc4, wc5, wc6, wc7]
+    P = Vh[-1,:] # P receives the last line of Vh
+    P = P.reshape(3,4)
+    return P
 
 
-if len(pixelCoords) != len(worldCoords):
-    print("Erro: Quantidade diferente de pontos.")
-else:	
-    numPoints = len(worldCoords)
+def main():    
+    """ Maracana2 - Pixel Coordinates: """
+
+    # PeqArea SupDir 473,99
+    pc1 = [99, 473]
+    # PeqArea SupEsq 508,177
+    pc2 = [177, 508]
+    # PeqArea InfDir 577,99
+    pc3 = [99, 577] 
+    # GraArea SupEsq 265,237
+    pc4 = [237, 265]
+    # GraArea SupDir 267,61
+    pc5 = [61, 267]
+    # GraArea InfDir 551,61
+    pc6 = [61, 551]
+    # Campo   InfDir 525,23
+    pc7 = [23, 525]
+    
+    pixelCoords = [pc1, pc2, pc3, pc4, pc5, pc6, pc7]
+    
+    
+    """ Maracana2 - World Coordinates: """
+    #Origem na trave direita, X paralelo à linha de fundo e Y paralelo à lateral
+    # wc = [x, y, x, w]
+    
+    # PeqArea SupDir
+    wc1 = [0, 5.5, 0, 1] 
+    # PeqArea SupEsq
+    wc2 = [-18.32, 5.5, 0, 1] 
+    # PeqArea InfDir 
+    wc3 = [0, 0, 0, 1]
+    # GraArea SupEsq
+    wc4 = [-29.32, 16.5, 0, 1] 
+    # GraArea SupDir 
+    wc5 = [11, 16.5, 0, 1]
+    # GraArea InfDir 
+    wc6 = [11, 0, 0, 1]
+    # Campo   InfDir 
+    wc7 = [24.84, 0, 0, 1]
+    
+    worldCoords = [wc1, wc2, wc3, wc4, wc5, wc6, wc7]
+
+    
+    """ Camera Resectioning - Finding the Camera Matrix P """
+    P = dlt(pixelCoords, worldCoords)
+    
+    if P == []:
+        return
+ 
+    # 2D -> 2D projection (Z=0)
+    P_2D = np.append(P[:,:2], P[:,3:], axis=1) # Elimination of column 3
+    P_2D_Inv = np.linalg.inv(P_2D)
+        
+        
+    """ Ex2 """
+    originalImg = cv2.imread('maracana2.jpg')
+    img = originalImg
+    cv2.namedWindow('image', flags=cv2.WINDOW_GUI_NORMAL)    # hides status, toolbar etc.
+    cv2.resizeWindow('image', img.shape[1], img.shape[0])
+    cv2.setMouseCallback('image', mouse_callback)
+
+    while window_is_open('image'):
+        cv2.imshow('image',img)
+        #if clicked:
+            # IMPLEMENTAR AQUI IDENTIFICACAO E PRINT DA LINHA
+            
+
+        if cv2.waitKey(20) == 27:
+            break
+    cv2.destroyAllWindows()
 
 
-""" Camera Resectioning - Finding the Camera Matrix P """  
-
-A = []
-
-for i in range(numPoints): 
-    x, y, z, w = worldCoords[i][0], worldCoords[i][1], worldCoords[i][2], worldCoords[i][3] 
-    u, v = pixelCoords[i][0], pixelCoords[i][1]
-    A.append([x, y, z, w, 0, 0, 0, 0, -u*x, -u*y, -u*z, -u])
-    A.append([0, 0, 0, 0, x, y, z, w, -v*x, -v*y, -v*z, -v])
-
-U, S, Vh = np.linalg.svd(A)
-
-P = Vh[-1,:] # P receives the last line of Vh
-P = P.reshape(3,4)
-
-
-
+if __name__ == "__main__":
+    main()
