d479d32c5618ae56d28068b0ce8829cc3bf1b1ae
Artur <arturwaquil@gmail.com>
2019-09-19 18:22:32 -0300

    just testing stuff

--- guitest.py
+++ guitest.py
@@ -8,34 +8,35 @@ def window_is_open(windowname):
 	return True if cv2.getWindowProperty(windowname, cv2.WND_PROP_VISIBLE) >= 1 else False
 
 
-
-def mouse_callback_mar2(event,x,y,flags,param):
+def mouse_callback(event,x,y,flags,param):
+	img = param
 	if event == cv2.EVENT_FLAG_LBUTTON:
-		cv2.circle(mar2,(x,y),10,(255,0,0),-1)
+		cv2.circle(img,(x,y),5,(0,0,255),-1)
+
+'''
+def mouse_callback_medidas(event,x,y,flags,param):
+	if event == cv2.EVENT_FLAG_LBUTTON:
+		cv2.circle(medidas,(x,y),10,(255,0,0),-1)
 	elif event == cv2.EVENT_FLAG_RBUTTON:
-		global oldx
-		global oldy
+		global oldx2
+		global oldy2
 		# green line from (oldx,oldy) to (x,y) with thickness=2 and antialiasing
-		cv2.line(mar2,(x,y),(oldx,oldy),(0,255,0),2,cv2.LINE_AA)
-		oldx = x
-		oldy = y
+		cv2.line(medidas,(x,y),(oldx2,oldy2),(0,255,0),2,cv2.LINE_AA)
+		oldx2 = x
+		oldy2 = y
+'''
 
-
-# the global vars oldx and oldy are needed to store previous mouse position
-oldx = 0
-oldy = 0		
-		
 mar2 = cv2.imread('maracana2.jpg')
-cv2.namedWindow('mar2', flags=cv2.WINDOW_GUI_NORMAL)	# hides status, toolbar etc.
+cv2.namedWindow('mar2', flags=cv2.WINDOW_GUI_NORMAL)
 cv2.resizeWindow('mar2', mar2.shape[1], mar2.shape[0])
-cv2.setMouseCallback('mar2', mouse_callback_mar2)
+cv2.setMouseCallback('mar2', mouse_callback, mar2)
 
 medidas = cv2.imread('maraca.png')
-medidas = cv2.resize(medidas, None, fx=0.25, fy=0.25)
-cv2.namedWindow('medidas do campo', flags=cv2.WINDOW_GUI_NORMAL)	# hides status, toolbar etc.
+medidas = cv2.resize(medidas, None, fx=0.3, fy=0.3)
+cv2.namedWindow('medidas do campo', flags=cv2.WINDOW_GUI_NORMAL)
 cv2.resizeWindow('medidas do campo', medidas.shape[1], medidas.shape[0])
 cv2.moveWindow('medidas do campo', 500,500)
-#cv2.setMouseCallback('medidas do campo', mouse_callback_medidas)
+cv2.setMouseCallback('medidas do campo', mouse_callback, medidas)
 
 
 while window_is_open('mar2') and window_is_open('medidas do campo'):
@@ -44,14 +45,3 @@ while window_is_open('mar2') and window_is_open('medidas do campo'):
 	if cv2.waitKey(20) == 27:
 		break
 cv2.destroyAllWindows()
-
-
-
-
-# PeqArea SupDir 473,99
-#         SupEsq 508,177
-#         InfDir 577,99 
-# GraArea SupEsq 265,237
-#         SupDir 267,61
-#         InfDir 551,61
-# Campo   InfDir 525,23
--- tp1.py
+++ tp1.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import DLT
 
 clicked = False
 cursorX = 0
@@ -7,110 +8,108 @@ cursorY = 0
 
 
 def mouse_callback(event,x,y,flags,param):
-    if event == cv2.EVENT_FLAG_LBUTTON:
-        clicked = True
-        global cursorX, cursorY
-        cursorX = x
-        cursorY = y        
-        
+	img, origImg = param
+	if event == cv2.EVENT_FLAG_LBUTTON:
+	#   img = origImg.copy()
+	#    cv2.line(img,(x,0),(x,img.shape[0]),(0,0,255),2,cv2.LINE_AA)
+	#elif event == cv2.EVENT_FLAG_RBUTTON:
+		global clicked, cursorX, cursorY
+		clicked = True
+		cursorX = x
+		cursorY = y
+		
 def window_is_open(windowname):
-    return True if cv2.getWindowProperty(windowname, cv2.WND_PROP_VISIBLE) >= 1 else False
+	return True if cv2.getWindowProperty(windowname, cv2.WND_PROP_VISIBLE) >= 1 else False
 
 #Direct Linear Transformation
 def dlt(pixelCoords, worldCoords):
-    if len(pixelCoords) != len(worldCoords):
-        print("Erro: Quantidade diferente de pontos.")
-        return []
-    else:    
-        numPoints = len(worldCoords)
+	if len(pixelCoords) != len(worldCoords):
+		print("Erro: Quantidade diferente de pontos.")
+		return None
+	else:    
+		numPoints = len(worldCoords)
 
-    A = []
+	#Tworld, worldCoords = Normalization(3, np.asarray(worldCoords))
+	#Tpixel, pixelCoords = Normalization(2, np.asarray(pixelCoords))
 
-    for i in range(numPoints): 
-        x, y, z, w = worldCoords[i][0], worldCoords[i][1], worldCoords[i][2], worldCoords[i][3] 
-        u, v = pixelCoords[i][0], pixelCoords[i][1]
-        A.append([x, y, z, w, 0, 0, 0, 0, -u*x, -u*y, -u*z, -u])
-        A.append([0, 0, 0, 0, x, y, z, w, -v*x, -v*y, -v*z, -v])
-        
-    U, S, Vh = np.linalg.svd(np.asarray(A))
+	A = []
 
-    P = Vh[-1,:] / Vh[-1,-1]  # The parameters are in the last line of Vh 
-    P = P.reshape(3,4)
-    print(P)
-    return P
+	for i in range(numPoints): 
+		x, y, z, w = worldCoords[i][0], worldCoords[i][1], worldCoords[i][2], 1
+		u, v = pixelCoords[i][0], pixelCoords[i][1]
+		A.append([x, y, z, w, 0, 0, 0, 0, -u*x, -u*y, -u*z, -u])
+		A.append([0, 0, 0, 0, x, y, z, w, -v*x, -v*y, -v*z, -v])
+		
+
+	#for i in A:
+	#    print(i)
+
+	U, S, Vh = np.linalg.svd(np.asarray(A))
+
+	P = Vh[-1,:] / Vh[-1,-1]  # The parameters are in the last line of Vh 
+	P = P.reshape(3,4)
+	'''
+	H = np.dot( np.dot( np.linalg.pinv(Tpixel), H ), Tworld )
+	print(H)
+	H = H / H[-1, -1]
+	print(H)
+	P = H.flatten(0)'''
+	print(P)
 
 
-def main():    
-    """ Maracana2 - Pixel Coordinates: """
+	return P
 
-    # PeqArea SupDir 473,99
-    pc1 = [99, 473]
-    # PeqArea SupEsq 508,177
-    pc2 = [177, 508]
-    # PeqArea InfDir 577,99
-    pc3 = [99, 577] 
-    # GraArea SupEsq 265,237
-    pc4 = [237, 265]
-    # GraArea SupDir 267,61
-    pc5 = [61, 267]
-    # GraArea InfDir 551,61
-    pc6 = [61, 551]
-    # Campo   InfDir 525,23
-    pc7 = [23, 525]
-    
-    pixelCoords = [pc1, pc2, pc3, pc4, pc5, pc6, pc7]
-    
-    
-    """ Maracana2 - World Coordinates: """
-    #Origem na trave direita, X paralelo à linha de fundo e Y paralelo à lateral
-    # wc = [x, y, x, w]
-    
-    # PeqArea SupDir
-    wc1 = [0, 5.5, 0, 1] 
-    # PeqArea SupEsq
-    wc2 = [-18.32, 5.5, 0, 1] 
-    # PeqArea InfDir 
-    wc3 = [0, 0, 0, 1]
-    # GraArea SupEsq
-    wc4 = [-29.32, 16.5, 0, 1] 
-    # GraArea SupDir 
-    wc5 = [11, 16.5, 0, 1]
-    # GraArea InfDir 
-    wc6 = [11, 0, 0, 1]
-    # Campo   InfDir 
-    wc7 = [24.84, 0, 0, 1]
-    
-    worldCoords = [wc1, wc2, wc3, wc4, wc5, wc6, wc7]
 
-    
-    """ Camera Resectioning - Finding the Camera Matrix P """
-    P = dlt(pixelCoords, worldCoords)
-    
-    if P == []:
-        return
+def main():
+
+
+	pixelCoords =  [[ 99, 473],	# PeqArea SupDir
+					[177, 508],	# PeqArea SupEsq
+					[ 99, 577],	# PeqArea InfDir
+					[237, 265],	# GraArea SupEsq 
+					[ 61, 267],	# GraArea SupDir
+					[ 61, 551],	# GraArea InfDir
+					[ 23, 525]]	# Campo   InfDir
+	
+	#Origem na trave direita, X paralelo à linha de fundo e Y paralelo à lateral
+	worldCoords =  [[     0,  5.5, 0],	# PeqArea SupDir
+					[-18.32,  5.5, 0],	# PeqArea SupEsq
+					[     0,    0, 0],	# PeqArea InfDir
+					[-29.32, 16.5, 0],	# GraArea SupEsq
+					[    11, 16.5, 0],	# GraArea SupDir
+					[    11,    0, 0],	# GraArea InfDir 
+					[ 24.84,    0, 0]]	# Campo   InfDir 
+	
+	""" Camera Resectioning - Finding the Camera Matrix P """
+	P = dlt(pixelCoords, worldCoords)
+	
+	if P is None:
+		return
  
-    # 2D -> 2D projection (Z=0)
-    P_2D = np.append(P[:,:2], P[:,3:], axis=1) # Elimination of column 3
-    P_2D_Inv = np.linalg.inv(P_2D)
-    
-        
-    """ Ex2 """
-    originalImg = cv2.imread('maracana2.jpg')
-    img = originalImg
-    cv2.namedWindow('image', flags=cv2.WINDOW_GUI_NORMAL)    # hides status, toolbar etc.
-    cv2.resizeWindow('image', img.shape[1], img.shape[0])
-    cv2.setMouseCallback('image', mouse_callback)
+	# 2D -> 2D projection (Z=0)
+	P_2D = np.append(P[:,:2], P[:,3:], axis=1) # Elimination of column 3
+	P_2D_Inv = np.linalg.inv(P_2D)
+	
+		
+	""" Ex2 """
+	origImg = cv2.imread('maracana2.jpg')
+	img = origImg.copy()
+	cv2.namedWindow('image', flags=cv2.WINDOW_GUI_NORMAL)    # hides status, toolbar etc.
+	cv2.resizeWindow('image', img.shape[1], img.shape[0])
+	cv2.setMouseCallback('image', mouse_callback, (img,origImg))
 
-    while window_is_open('image'):
-        cv2.imshow('image',img)
-        #if clicked:
-            # IMPLEMENTAR AQUI IDENTIFICACAO E PRINT DA LINHA
-            
+	while window_is_open('image'):
+		cv2.imshow('image',img)
 
-        if cv2.waitKey(20) == 27:
-            break
-    cv2.destroyAllWindows()
+		if clicked:
+			# IMPLEMENTAR AQUI IDENTIFICACAO E PRINT DA LINHA
+			img = origImg.copy()
+			cv2.line(img,(cursorX,0),(cursorX,img.shape[0]),(0,0,255),2,cv2.LINE_AA)
+
+		if cv2.waitKey(20) == 27:
+			break
+	cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    main()
+	main()
